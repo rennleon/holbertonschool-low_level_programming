f6f9e41c6f21cbca740eab3d367c8fe04a1f476b
renatoleonholberton <2829@holbertonschool.com>
2021-02-03 22:54:10 +0000

    Include <stdlib.h> to use "exit" function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry point
  *
@@ -9,5 +10,5 @@ int main(void)
 	char m[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	fwrite(m, 60, 1, stderr);
-	return (1);
+	exit(EXIT_FAILURE);
 }
