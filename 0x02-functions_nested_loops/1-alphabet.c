519b205e888912b37eebaefd647767a3843ad726
renatoleonholberton <2829@holbertonschool.com>
2021-02-09 02:03:22 +0000

    Create a function that prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_alphabet - print th alphabet in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet()
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+
+	_putchar('\n');
+}
