56dadce99a99d9e2a717c390928119f4d0a8ecf5
renatoleonholberton <2829@holbertonschool.com>
2021-02-10 03:06:17 +0000

    Add function two print fib numbers in two halves to avoid overflow

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void print_overflowed_fib(unsigned long, unsigned long);
+
 /**
  * main - Entry point
  *
@@ -8,24 +10,76 @@
 int main(void)
 {
 	int i;
-	double n1, n2;
-	double sum;
+	unsigned long n1, n2;
+	unsigned long fib;
 
 	n1 = 0;
 	n2 = 1;
 
-	for (i = 0; i < 97; i++)
+	/* Only prints well the first 77 nums without overflow */
+	for (i = 0; i <= 76; i++)
 	{
-		sum = n1 + n2;
+		fib = n1 + n2;
 
-		printf("%.0f, ", sum);
+		printf("%lu, ", fib);
 
 		n1 = n2;
-		n2 = sum;
+		n2 = fib;
 	}
 
-	printf("%.0f\n", (n1 + n2));
+	print_overflowed_fib(n1, n2);
+
+	printf("\n");
 
 	return (0);
 }
 
+/**
+ * print_overflowed_fib - Prints the fiboacci numbers in two halves
+ * to avoid overflow
+ * @n1: penultimate fib without overflow
+ * @n2: last fib without overflow.
+ */
+void print_overflowed_fib(unsigned long n1, unsigned long n2)
+{
+	int i;
+	unsigned long n1_half1, n1_half2;
+	unsigned long n2_half1, n2_half2;
+	unsigned long fib_half1, fib_half2;
+
+	unsigned long divisor;
+
+	/* Start printing the resting 21 fib numbers */
+	/* numbers are 21 digits long */
+	divisor = 10000000000;
+	n1_half1 = n1 / divisor;
+	n1_half2 = n1 % divisor;
+
+	n2_half1 = n2 / divisor;
+	n2_half2 = n2 % divisor;
+
+	for (i = 0; i <= 20; i++)
+	{
+		fib_half2 = n1_half2 + n2_half2;
+		fib_half1 = n1_half1 + n2_half1;
+
+		if (fib_half2 > divisor - 1)
+		{
+			/* get rid of the excess unit */
+			fib_half2 %= divisor;
+			/* add the excess to first half */
+			fib_half1 += 1;
+		}
+
+		printf("%lu%010lu", fib_half1, fib_half2);
+
+		if (i < 20)
+			printf(", ");
+
+		n1_half1 = n2_half1;
+		n1_half2 = n2_half2;
+
+		n2_half1 = fib_half1;
+		n2_half2 = fib_half2;
+	}
+}
