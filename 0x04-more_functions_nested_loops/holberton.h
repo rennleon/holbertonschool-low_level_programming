74463112e361d2cf1a9bccdafbe107af1f0b0cd2
renatoleonholberton <2829@holbertonschool.com>
2021-02-11 14:35:08 +0000

    Create holberton.h header

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,31 @@
+int _putchar(char);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+void print_one_digit(int res, int j);
+void print_two_digits(int res);
+void print_three_digits(int res);
+
+/* 0x04 prototypes */
+int _isupper(int c);
