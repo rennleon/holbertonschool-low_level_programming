ce196590a6acb895d7dbe44bcc5f353cde2f8140
renatoleonholberton <2829@holbertonschool.com>
2021-02-15 22:59:31 +0000

    Delete * from param description

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - Update the value of n to 98
- * @*n: memory adress which value will be updated to 98
+ * @n: memory adress which value will be updated to 98
  */
 void reset_to_98(int *n)
 {
