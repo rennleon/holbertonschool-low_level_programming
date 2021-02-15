424e7a900d2d492ebb77f8fc6ab6499eb359f7bf
renatoleonholberton <2829@holbertonschool.com>
2021-02-15 23:17:54 +0000

    Create a function that returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "holberton.h"
+
+/**
+ * _strlen - Calculates and returns the lenght of a string
+ * @s: String which length will be calculated
+ *
+ * Return: Length of "s"
+ */
+int _strlen(char *s)
+{
+	return strlen(s);
+}
