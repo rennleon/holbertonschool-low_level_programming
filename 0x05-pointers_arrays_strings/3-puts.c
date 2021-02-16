06a06bfa064e88945e4a6d9a604eb1b374d92ccc
renatoleonholberton <2829@holbertonschool.com>
2021-02-16 14:25:44 +0000

    Add logic to print a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "holberton.h"
 
 /**
@@ -7,5 +6,12 @@
  */
 void _puts(char *str)
 {
-	puts(str);
+	int len;
+
+	len = 0;
+
+	while (*(str + len) != '\0')
+		_putchar(*(str + len++));
+
+	_putchar('\n');
 }
