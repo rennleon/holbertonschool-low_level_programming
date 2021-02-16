3a83deebc8c1dfd0a648f659fae7d12ce129fc45
renatoleonholberton <2829@holbertonschool.com>
2021-02-16 17:41:38 +0000

    Create a function that converts a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,57 @@
+#include "holberton.h"
+
+int is_digit(char c);
+int is_alpha(char c);
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: String to be converted to integer
+ */
+int _atoi(char *s)
+{
+	int sign;
+	int value;
+
+	sign = 1;
+	value = 0;
+
+	while (*s != '\0')
+	{
+		if (is_digit(*s))
+		{
+			value *= 10;
+			/* Get int value instead of ascii code */
+			value += (*s) - '0';
+		}
+		else if (*s == '-')
+			sign *= -1;
+		else if (value > 0 && is_alpha(*s))
+			return (value * sign);
+
+		s++;
+	}
+
+	return (value * sign);
+}
+
+/**
+ * is_digit - Evaluates if a char is a digit
+ * @c: char to be evaluated
+ *
+ * Return: 1 if c is digit, 0 otherwise
+ */
+int is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+/**
+ * is_alpha - Evaluates if a char is a letter
+ * @c: char to be evaluated
+ *
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int is_alpha(char c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
