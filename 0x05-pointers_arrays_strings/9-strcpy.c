4187e61047c780ee77dc58889a03251757211af4
renatoleonholberton <2829@holbertonschool.com>
2021-02-16 00:39:14 +0000

    Create a function that copies the string pointed to by src, to the buffer pointed to by dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "holberton.h"
+
+/**
+ * _strcpy - Copies the string src to dest
+ * @dest: Destiny pointer
+ * @src: String source
+ *
+ * Return: Pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return strcpy(dest, src);
+}
