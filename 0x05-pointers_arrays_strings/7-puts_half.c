adf5f2ade4b1eaacf6d03d8cd86c8d220235b407
renatoleonholberton <2829@holbertonschool.com>
2021-02-16 14:45:53 +0000

    Add logic to find the length of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
-#include <string.h>
 #include "holberton.h"
 
 /**
  * puts_half - Prints second half of a string
- * @str: String which second half will be printed 
+ * @str: String which second half will be printed
  */
 void puts_half(char *str)
 {
@@ -12,15 +10,18 @@ void puts_half(char *str)
 	int len;
 	int start;
 
-	len = strlen(str);
+	len = 0;
+
+	while (*(str + len) != '\0')
+		len++;
 
 	start = len / 2;
 
 	if (len % 2 != 0)
 		start++;
 
-	for(i = start; i < len; i++)
-		putchar(str[i]);
+	for (i = start; i < len; i++)
+		_putchar(str[i]);
 
-	putchar('\n');
+	_putchar('\n');
 }
