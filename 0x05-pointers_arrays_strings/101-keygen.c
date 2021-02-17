09c5c41d55ac872e8524e04fbf32e57cbb0ee4e1
renatoleonholberton <2829@holbertonschool.com>
2021-02-17 04:00:56 +0000

    Create a C program that generates random valid passwords for the program 101-crackme

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void substract_key(char key[], int len, int checksum);
+
+/**
+ * main - Generates a random password
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	/* 2772 */
+	char c;
+	char key[60];
+	int i;
+	int r_val;
+	int checksum;
+
+	i = 0;
+	r_val = rand();
+	checksum = 0;
+
+	while (checksum < 2772)
+	{
+		c = '0' + (r_val % 10);
+		checksum += c;
+		r_val /= 10;
+		key[i++] = c;
+
+		if (r_val == 0)
+			r_val = rand();
+	}
+
+	key[i++] = '\0';
+
+	substract_key(key, i, checksum);
+
+	printf("%s", key);
+
+	return (0);
+}
+
+/**
+ * substract_key - Rests one to each char of string
+ * @key: key to be printed as output
+ * @len: length of key
+ * @checksum: Sum of all the ascii codes in key
+ *
+ */
+void substract_key(char key[], int len, int checksum)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < len && checksum != 2772; i++)
+	{
+		c = key[i] - 1;
+		if (c >= '0' && c <= '9')
+		{
+			key[i] = c;
+			checksum--;
+		}
+	}
+}
