4a3136fc7eed7ab91777709dfab7611f21790895
renatoleonholberton <2829@holbertonschool.com>
2021-02-18 15:19:08 +0000

    Create a function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strcat - Appends src to dest and returns a pointer to dest
+ * @dest: String to be extended
+ * @src: String to be appended to dest
+ *
+ * Return: Pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int offset = 0;
+	char *start = dest;
+
+	while (*(start + offset) != '\0')
+		offset++;
+
+	while (*(src) != '\0')
+	{
+		*(start + offset) = *(src);
+		offset++, src++;
+	}
+
+	*(start + offset) = '\0';
+
+	return (dest);
+}
