0ada7275da8fae154e356f673d45810605b8ebe7
renatoleonholberton <2829@holbertonschool.com>
2021-02-24 19:47:48 +0000

    Format code to pass betty

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,15 +3,17 @@
 /**
  * factorial - Calculates the factorial of a given number
  * @n: Number to calcute the factorial of
+ *
+ * Return: Factorial of n, -1 if negative
  */
 int factorial(int n)
 {
 	if (n < 0)
-		return -1;
+		return (-1);
 
 	/* Case for n=0 or n=1 */
 	if (n <= 1)
-		return 1;
+		return (1);
 
-	return n * factorial(n - 1);
+	return (n * factorial(n - 1));
 }
