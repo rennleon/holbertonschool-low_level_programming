fb8d4016e42ec2257da818c37d1883e04769c19b
renatoleonholberton <2829@holbertonschool.com>
2021-02-24 19:53:23 +0000

    Format code to pass betty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,31 +11,34 @@ int newton_sqrt(int, int);
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-		return -1;
+		return (-1);
 
 	if (n <= 1)
-		return n;
+		return (n);
 
-	return newton_sqrt(n, n/2);
+	return (newton_sqrt(n, n / 2));
 }
 
 /**
  * newton_sqrt - Calculates the sqrt root of n based on Newton's rule
  * @n: Num to calculate the sqrt root
  * @x: Guess of sqrt root
+ *
+ * Return: Natural sqrt root of n
  */
 int newton_sqrt(int n, int x)
 {
 	int root = (x + (n / x)) / 2;
 
 	if (root * root > n)
-		return newton_sqrt(n, root);
+		return (newton_sqrt(n, root));
 
 	if (root * root == n)
-		return root;
+		return (root);
 
-	/* If the root^2 is less than n means that n
+	/*
+	 * If the root^2 is less than n means that n
 	 * doesn't have natural square root
 	 */
-	return -1;
+	return (-1);
 }
