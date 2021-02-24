69d7e9fe20deb0dc5dc724951988160ce2122bd5
renatoleonholberton <2829@holbertonschool.com>
2021-02-24 19:50:54 +0000

    Format code to pass betty

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,17 +4,19 @@
  * _pow_recursion - Calculates the power x raised to y
  * @x: Base number
  * @y: Power number
+ *
+ * Return: Power of x raised to y
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return -1;
+		return (-1);
 
 	if (y == 0)
-		return 1;
+		return (1);
 
 	if (y == 1)
-		return x;
+		return (x);
 
-	return x * _pow_recursion(x, y - 1);
+	return (x * _pow_recursion(x, y - 1));
 }
