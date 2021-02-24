6a6482baf4cff08d0e163e329d1467b20d8e0df7
renatoleonholberton <2829@holbertonschool.com>
2021-02-24 19:44:58 +0000

    Format code to pass betty

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,11 +3,13 @@
 /**
  * _strlen_recursion - Calculates the length of a string
  * @s: String of chars
+ *
+ * Return: Lenght of string s
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return 0;
+		return (0);
 
-	return 1 + _strlen_recursion(s + 1);
+	return (1 + _strlen_recursion(s + 1));
 }
