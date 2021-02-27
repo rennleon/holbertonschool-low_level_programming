ef6c6c13eeb1533d1b36a0958a66dca64e51d486
renatoleonholberton <2829@holbertonschool.com>
2021-02-27 03:17:41 +0000

    Create a program that adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int is_number(char *);
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of strings that are the actual arguments passed to the program
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	unsigned long sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (is_number(argv[i]))
+			sum += atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	printf("%ld\n", sum);
+
+	return (0);
+}
+
+int is_number(char *s)
+{
+	while (*s != '\0')
+	{
+		if (*s < '0' || *s > '9')
+			return (0);
+		s++;
+	}
+
+	return (1);
+}
