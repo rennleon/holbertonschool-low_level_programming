fbb620a19c5feeec45198dd2b987832e2a436f49
renatoleonholberton <2829@holbertonschool.com>
2021-03-02 23:55:42 +0000

    Move for loop to initialize poritions right after creating a row

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -30,11 +30,10 @@ int **alloc_grid(int width, int height)
 
 		if (matrix[i] == NULL)
 			return (NULL);
-	}
-
-	for (i = 0; i < height; i++)
+	
 		for (j = 0; j < width; j++)
 			matrix[i][j] = 0;
+	}
 
 	return (matrix);
 }
