3b4bda7962ee32eee6561ecd860a37d43f95d370
renatoleonholberton <2829@holbertonschool.com>
2021-03-03 00:46:59 +0000

    Add logic to free allocated memory when out of memory happens

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void free_previous_malloc(int **matrix, int index);
+
 /**
  * alloc_grid - Creates a two dimensional array
  * @width: Width of the matrix
@@ -29,11 +31,27 @@ int **alloc_grid(int width, int height)
 		matrix[i] = malloc(width * sizeof(int));
 
 		if (matrix[i] == NULL)
+		{
+			free_previous_malloc(matrix, i - 1);
+			free(matrix);
 			return (NULL);
+		}
 
 		for (j = 0; j < width; j++)
 			matrix[i][j] = 0;
 	}
 
 	return (matrix);
+}
+
+/**
+ * free_previous_malloc - Frees rows that had been allocated
+ * before the out of memory happened
+ * @matrix: matrix who's rows are going to be freed
+ * @index: End row index to start freeing memory
+ */
+void free_previous_malloc(int **matrix, int index)
+{
+	while (index >= 0)
+		free(matrix[index--]);
 }
