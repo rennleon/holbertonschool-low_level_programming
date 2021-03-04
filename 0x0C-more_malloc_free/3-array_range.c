446fb0b8a6914a232f25bdbd556c84594b9c9a56
renatoleonholberton <2829@holbertonschool.com>
2021-03-04 19:21:12 +0000

    Add parenthesis to sizeof to pass betty style

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 		return (NULL);
 
 	len = max - min + 1;
-	arr = malloc(len * sizeof *arr);
+	arr = malloc(len * sizeof(*arr));
 
 	if (arr == NULL)
 		return (NULL);
