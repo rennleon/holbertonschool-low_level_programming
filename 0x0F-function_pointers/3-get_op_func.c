07ad743896aa148981b7ce5e7bcbf1df5fb659c1
renatoleonholberton <2829@holbertonschool.com>
2021-03-10 18:39:03 +0000

    Create a program that performs simple arithmetic operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include "3-calc.h"
+
+int (*get_op_func(char *s))(int, int)
+{
+	int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return ops[i].f;
+
+		i++;
+	}
+
+	return NULL;
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "3-calc.h"
+
+int main(int argc, char **argv)
+{
+	int (*oper)(int, int) = NULL;
+	int n1 = 0, n2 = 0;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	oper = get_op_func(argv[2]);
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+
+	if (oper == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0) && n2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", oper(n1, n2));
+
+	return (0);
+}
