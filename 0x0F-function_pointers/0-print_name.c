b8995db757d8e8a2f2ff51ca2ac2c8cbeef80dde
renatoleonholberton <2829@holbertonschool.com>
2021-03-10 18:58:17 +0000

    Add description to pass betty

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+/**
+ * print_name - Executes f with name as argument
+ * @name: String to be passed as argument to f
+ * @f: pointer to a function
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
