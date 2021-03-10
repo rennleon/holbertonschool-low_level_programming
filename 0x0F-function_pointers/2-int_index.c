6367a32ead8b256e40c78cd48a5fe03cebe4699f
renatoleonholberton <2829@holbertonschool.com>
2021-03-10 19:08:25 +0000

    Add description to pass betty

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,6 +1,14 @@
 #include <stddef.h>
 #include "function_pointers.h"
 
+/**
+ * int_index - Apllies cmp function to each array node and returns the index
+ * @array: Array of integers
+ * @size: Ssixe of array
+ * @cmp: Pointer to a function
+ *
+ * Return: Index of the element if cmp(array[ind]) == 0, -1 otherwise
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
