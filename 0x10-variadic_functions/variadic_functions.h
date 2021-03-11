a5854c470f477a2c2d5055f9b55d9d2eb7e66175
renatoleonholberton <2829@holbertonschool.com>
2021-03-11 23:40:57 +0000

    Add structure and typedef for fmt

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -12,4 +12,16 @@ void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
 
+/**
+ * struct fmt - Structure to mathc char format to function to print
+ * @in: Char representing the format
+ * @fn: Function pointer to print an arg
+ */
+typedef struct fmt
+{
+	char in;
+	void (*fn)(va_list);
+} fmt;
+
+
 #endif /* VARIADIC_FUNC_H */
