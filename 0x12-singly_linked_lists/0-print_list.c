41f2066cb594759bdcb690b334ad8771d65ce3cd
renatoleonholberton <2829@holbertonschool.com>
2021-03-18 21:29:43 +0000

    Create file with vim to pass betty

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "list.h"
+
+/**
+ * print_list - Prints the elements of a singly linked list
+ * @h: Pointer to list head
+ *
+ * Return: Number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int coun = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%d] %s", h->len, h->str);
+
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
