2b3c92c9058eeabd9a938d0a41d648ae8dbcf7ae
renatoleonholberton <2829@holbertonschool.com>
2021-03-18 22:41:25 +0000

    Create a function that prints a message before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void print_before_main (void) __attribute__ ((constructor));
+/**
+ * print_before_main - Executes it's code before masin is executed
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
