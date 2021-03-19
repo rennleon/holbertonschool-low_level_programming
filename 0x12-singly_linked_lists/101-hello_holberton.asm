4bde45db006fa5b1f2b452fdefc4155dc8a8aeb0
renatoleonholberton <2829@holbertonschool.com>
2021-03-19 01:44:29 +0000

    Add return value 0

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,16 @@
 section .text
+	extern printf
 	global main
 
 main:
 	mov rdi,formatStr ;1st arg
 	mov rsi,5 				;2nd arg
-	;mov al,0 					;magic for varargs (0==no magic)
-	mov rax, 0
+	mov al,0 					;magic for varargs (0==no magic)
 
-	extern printf
 	call printf
-
+	
+	mov rax, 0
 	ret
 
 section .data
-	formatStr db `Hello, %d Holberton\n`,0
+	formatStr db `Hello, Holberton\n`,0
