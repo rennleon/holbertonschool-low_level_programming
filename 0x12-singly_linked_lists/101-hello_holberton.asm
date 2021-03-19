12ade2d4d169a0baa0fb6bf8630a259298060937
renatoleonholberton <2829@holbertonschool.com>
2021-03-19 01:40:46 +0000

    Use rax register

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,13 +3,14 @@ section .text
 
 main:
 	mov rdi,formatStr ;1st arg
-	;mov rsi,5 				;2nd arg
-	mov al,0 					;magic for varargs (0==no magic)
+	mov rsi,5 				;2nd arg
+	;mov al,0 					;magic for varargs (0==no magic)
+	mov rax, 0
 
 	extern printf
 	call printf
 
 	ret
 
-formatStr:
-	db `Hello, Holberton\n`,0
+section .data
+	formatStr db `Hello, %d Holberton\n`,0
