3277d4dff2bbc0b185150b7aa506356a749f1002
renatoleonholberton <2829@holbertonschool.com>
2021-03-19 01:51:03 +0000

    Delete unsuded line

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,14 +3,13 @@ section .text
 	global main
 
 main:
-	mov rdi,formatStr ;1st arg
-	mov rsi,5 				;2nd arg
-	mov al,0 					;magic for varargs (0==no magic)
+	mov rdi, msg
+	mov al, 0
 
 	call printf
-	
+
 	mov rax, 0
 	ret
 
 section .data
-	formatStr db `Hello, Holberton\n`,0
+	msg db `Hello, Holberton\n`,0
