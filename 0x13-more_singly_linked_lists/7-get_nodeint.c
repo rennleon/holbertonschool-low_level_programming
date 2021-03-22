0ca6d949b16e5a5e98422853ae93185dd758d602
renatoleonholberton <2829@holbertonschool.com>
2021-03-22 15:15:56 +0000

    Reduce logic

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -17,8 +17,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	for (i = 0; i < index && head != NULL; i++)
 		head = head->next;
 
-	if (head != NULL)
-		return (head);
-
-	return (NULL);
+	return (head);
 }
