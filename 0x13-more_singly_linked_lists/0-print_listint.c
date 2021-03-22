3d9e7987c0174dbb53d2d9487bbafaf70221b8c2
renatoleonholberton <2829@holbertonschool.com>
2021-03-22 13:48:12 +0000

    Format code to pass betty's style

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,18 @@
 #include "lists.h"
 
+/**
+ * print_listint - Prints linked list's nodes values
+ * @h: Pointer list's head node
+ *
+ * Return: Number of nodes
+ */
 size_t print_listint(const listint_t *h)
 {
 	int count_nodes = 0;
 
 	if (h == NULL)
 		return (0);
-	
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
