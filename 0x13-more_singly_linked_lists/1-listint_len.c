8a6ad78780f921df49a122f83374a73115536b00
renatoleonholberton <2829@holbertonschool.com>
2021-03-22 13:49:55 +0000

    Format code to pass betty's style

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,18 @@
 #include "lists.h"
 
+/**
+ * listint_len - Counts the ndes of a list
+ * @h: Pointer to the list's head node
+ *
+ * Return: Number of nodes
+ */
 size_t listint_len(const listint_t *h)
 {
 	int count_nodes = 0;
 
 	if (h == NULL)
 		return (0);
-	
+
 	while (h != NULL)
 	{
 		count_nodes++;
