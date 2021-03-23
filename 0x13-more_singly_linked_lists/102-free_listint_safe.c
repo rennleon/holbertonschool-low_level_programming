813f142a21debb0241e4a178dd2dfb5b269169eb
renatoleonholberton <2829@holbertonschool.com>
2021-03-23 20:17:15 +0000

    Improve logic to free memory

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 
+listint_t *intersect_node(listint_t *curr);
+
 /**
  * free_listint_safe - Frees the memory used by a list
  * @h: Pointer to pointer to head node
@@ -8,25 +10,75 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	int count = 0;
-	listint_t *prev, *curr;
+	int count = 0, passed_int_node = 0;
+	listint_t *tmp, *curr;
+	listint_t *int_node;
 
 	if (h == NULL || *h == NULL)
 		return (0);
 
 	curr = *h;
 
+	int_node = intersect_node(curr);
+
 	while (curr != NULL)
 	{
-		count++;
-		prev = curr;
+		tmp = curr;
 		curr = curr->next;
-		prev->next = NULL;
-		free(prev);
+		if (tmp != int_node)
+		{
+			count++;
+			tmp->next = NULL;
+			free(tmp);
+		}
+		else
+			passed_int_node++;
+
+		if (passed_int_node > 1)
+		{
+			tmp->next = NULL;
+			free(tmp);
+			return (++count);
+		}
 	}
 
-	free(*h);
+	/*free(*h);*/
 	*h = NULL;
 
 	return (count);
+}
+
+/**
+ * intersect_node - Finds the interseccion node of a cyclic linked list
+ * @head: Pointer to head node
+ *
+ * Return: Pointer to the intersection node or NULL if it doen't exists
+ */
+listint_t *intersect_node(listint_t *head)
+{
+	int cmp_slow_curr = 0;
+	listint_t *slow, *fast, *curr;
+
+	slow = fast = curr = head;
+
+	while (curr != NULL)
+	{
+		curr = curr->next;
+
+		slow = slow->next;
+		if (cmp_slow_curr == 0 && fast && fast->next)
+		{
+			fast = fast->next->next;
+			if (slow == fast)
+			{
+				cmp_slow_curr = 1;
+				slow = head;
+			}
+		}
+
+		if (cmp_slow_curr && slow == curr)
+			return (curr);
+	}
+
+	return (NULL);
 }
