cebbe6b5fa2dec1e96bbf8c15421adc83ccf4df5
renatoleonholberton <2829@holbertonschool.com>
2021-03-23 20:31:20 +0000

    Add statement to free memory after removing head's node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,12 +9,15 @@
 int pop_listint(listint_t **head)
 {
 	int data;
+	listint_t *tmp;
 
 	if (head == NULL || *head == NULL)
 		return (0);
 
+	tmp = *head;
 	data = (*head)->n;
 	*head = (*head)->next;
+	free(tmp);
 
 	return (data);
 }
