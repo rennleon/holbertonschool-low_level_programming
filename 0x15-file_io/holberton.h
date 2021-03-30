d2b4c748708223c612db76ebfe189e74f914a96b
renatoleonholberton <2829@holbertonschool.com>
2021-03-30 18:09:37 +0000

    Create holberton.h header with initial prototypes

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char);
+
+/* 0x15 prototypes */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* HOLBERTON_H */
