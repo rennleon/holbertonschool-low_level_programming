4131380c575f3dafd618b6a33e75b52b7548122e
renatoleonholberton <2829@holbertonschool.com>
2021-04-01 02:51:42 +0000

    Update print_magic code

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -109,10 +109,10 @@ void print_magic(unsigned char *e_ident)
 
 	printf("  Magic:   ");
 
-	for (i = 0; i < EI_NIDENT - 1; i++)
-		printf("%02x ", e_ident[i]);
+	for (i = 0; i < EI_NIDENT; i++)
+		printf("%2.2x ", e_ident[i]);
 
-	printf("%02x\n", e_ident[EI_NIDENT - 1]);
+	printf("\n");
 }
 
 /**
