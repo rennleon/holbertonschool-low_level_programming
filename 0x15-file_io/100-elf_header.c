1766814e584be9942ebb1026bffd29377563271c
renatoleonholberton <2829@holbertonschool.com>
2021-04-01 03:24:27 +0000

    Revert changes to print magic

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -109,13 +109,10 @@ void print_magic(unsigned char *e_ident)
 
 	printf("  Magic:   ");
 
-	for (i = 0; i < EI_NIDENT; i++)
-	{
-		printf("%2.2x", e_ident[i]);
-		if (i < EI_NIDENT - 1)
-			printf(" ");
-	}
-	printf("\n");
+	for (i = 0; i < EI_NIDENT - 1; i++)
+		printf("%02x ", e_ident[i]);
+
+	printf("%02x\n", e_ident[EI_NIDENT - 1]);
 }
 
 /**
