9f249c44dca65b53b658590e91575cb4d3edf188
renatoleonholberton <2829@holbertonschool.com>
2021-05-02 07:34:37 +0000

    Aalter order of pointers asignment

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,11 +31,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node = malloc(sizeof(*new_node));
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->n = n;
 	new_node->next = prev->next;
 	new_node->prev = prev;
-	prev->next = new_node;
 	prev->next->prev = new_node;
+	prev->next = new_node;
 
 	return (new_node);
 }
