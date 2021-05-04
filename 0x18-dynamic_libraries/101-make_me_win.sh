1af534808c8f53811a536ae48b37531870363e16
renatoleonholberton <2829@holbertonschool.com>
2021-05-04 01:05:43 +0000

    Add -O flag to download library in tmp dir

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://raw.githubusercontent.com/renatoleonholberton/holbertonschool-low_level_programming/main/0x18-dynamic_libraries/libnewrand.so
+wget -O /tmp/libnewrand.so https://raw.githubusercontent.com/renatoleonholberton/holbertonschool-low_level_programming/main/0x18-dynamic_libraries/libnewrand.so
 export LD_PRELOAD=/tmp/libnewrand.so
