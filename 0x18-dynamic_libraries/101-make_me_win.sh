8cd7040925e10cb0dfb4ce2bd6ac1f4619ffd905
renatoleonholberton <2829@holbertonschool.com>
2021-05-04 01:02:51 +0000

    Modify URL to use download without modifying the ELF header of the library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/renatoleonholberton/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/libnewrand.so
+wget -P /tmp https://raw.githubusercontent.com/renatoleonholberton/holbertonschool-low_level_programming/main/0x18-dynamic_libraries/libnewrand.so
 export LD_PRELOAD=/tmp/libnewrand.so
