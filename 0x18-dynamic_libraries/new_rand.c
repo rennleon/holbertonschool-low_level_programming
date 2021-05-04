07ed0fca4eb72d6fd609259be68d8a683bcafc00
renatoleonholberton <2829@holbertonschool.com>
2021-05-04 03:51:00 +0000

    Update code to reduce logic

--- 0x18-dynamic_libraries/new_rand.c
+++ 0x18-dynamic_libraries/new_rand.c
@@ -1,22 +1,12 @@
 int rand(void);
-
+const int arr[] = {8, 8, 7, 9, 34, 74};
 static int ind = 0;
 int rand(void)
 {
 	ind++;
 
-	if (ind == 1)
-		return 8;
-	if (ind == 2)
-		return 8;
-	if (ind == 3)
-		return 7;
-	if (ind == 4)
-		return 9;
-	if (ind == 5)
-		return 23;
-	if (ind == 6)
-		return 74;
+	if (ind <= 6)
+		return arr[ind - 1];
 
 	return (77 + ind++);
 }
