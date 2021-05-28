533dd808a62819e697a9f3f1575ee5ad326def9e
renatoleonholberton <2829@holbertonschool.com>
2021-05-28 16:59:44 +0000

    Aadd logic to search for key to retrieve the associated value

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,6 +9,7 @@
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	hash_node_t *curr;
 	unsigned long int index, hash;
 
 	if (ht == NULL || ht->array == NULL || key == NULL || *key == '\0')
@@ -20,5 +21,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if ((ht->array)[index] == NULL)
 		return (NULL);
 
-	return ((ht->array)[index]->value);
+	for (curr = (ht->array)[index]; curr != NULL; curr = curr->next)
+		if (strcmp(curr->key, key) == 0)
+			return (curr->value);
+
+	return (NULL);
 }
