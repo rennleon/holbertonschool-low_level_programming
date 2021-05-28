29f8c221a1b5dd6f7cd39d0cd86594b17b11acdd
renatoleonholberton <2829@holbertonschool.com>
2021-05-28 01:13:13 +0000

    Create a function that adds an element to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table
+ * @ht: Pointer t the hash table
+ * @key: String from which to calculate the hash
+ * @value: String to be stored in the hash table
+ *
+ * Return: 1 on success, 0 otherwise
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t **array, *curr, *new_node;
+	unsigned long int index, hash;
+
+	if (ht == NULL || ht->array == NULL || key == NULL || *key == '\0')
+		return (0);
+
+	new_node = malloc(sizeof(*new_node));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+
+	array = ht->array;
+	hash = hash_djb2((const unsigned char *)key);
+	index = hash % ht->size;
+
+	if (array[index] == NULL)
+		array[index] = new_node;
+	else
+	{
+		curr = array[index];
+		while (curr->next != NULL)
+			curr = curr->next;
+		curr->next = new_node;
+	}
+
+	return (1);
+}
