5b57404b4510a1509a71df5547a6ca40544d615d
renatoleonholberton <2829@holbertonschool.com>
2021-05-28 00:41:39 +0000

    Add code to initialize a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,5 +8,20 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	return (malloc(sizeof(hash_table_t) * size));
+	hash_table_t *ht;
+	unsigned long int i;
+
+	ht = malloc(sizeof(*ht));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(*(ht->array)) * size);
+	if (ht->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		(ht->array)[i] = NULL;
+
+	return (ht);
 }
