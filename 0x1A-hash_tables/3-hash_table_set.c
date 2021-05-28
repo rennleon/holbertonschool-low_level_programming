d5777ed2555d824568adc9d664a3741c6a15cd27
renatoleonholberton <2829@holbertonschool.com>
2021-05-28 16:54:03 +0000

    Add logic to handle duplicated key cases

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,11 +10,22 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
+	hash_node_t *new_node, *curr;
 	unsigned long int index, hash;
 
 	if (ht == NULL || ht->array == NULL || key == NULL || *key == '\0')
 		return (0);
+
+	hash = hash_djb2((const unsigned char *)key);
+	index = hash % ht->size;
+
+	for (curr = ht->array[index]; curr != NULL; curr = curr->next)
+		if (strcmp(curr->key, key) == 0)
+		{
+			free(curr->value);
+			curr->value = strdup(value);
+			return (1);
+		}
 
 	new_node = malloc(sizeof(*new_node));
 	if (new_node == NULL)
@@ -22,11 +33,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = NULL;
-
-	hash = hash_djb2((const unsigned char *)key);
-	index = hash % ht->size;
-
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
