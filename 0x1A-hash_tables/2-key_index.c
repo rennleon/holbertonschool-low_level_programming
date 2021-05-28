a5129036e25a17fca4796fa5df9f4efa4ad9e221
renatoleonholberton <2829@holbertonschool.com>
2021-05-28 00:22:04 +0000

    Create a function that gives you the index of a key

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Returns the index at wich a string should
+ *             be placed into in the hash table
+ * @key: String value from which to calculate the hash
+ * @size: Hassh tables' size
+ *
+ * Return: Index at wich the value should be placed into the hash table
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
