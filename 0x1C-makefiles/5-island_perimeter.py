e1f498e45c3904840a5e1afc9b0079ba4490482a
renatoleon <2829@holbertonschool.com>
2021-07-03 08:23:14 +0000

    Refactor code

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,55 +1,42 @@
 #!/usr/bin/python3
 """ This modules defines a function to get an island perimeter """
 
-
-def create_calc_cell(grid, width, height):
-    """ Injects grid's dimensions """
-    def cell_perimeter(row, col):
-        """ Calculates the perimeter of a cell """
-        cell = 4
-        if row - 1 >= 0 and grid[row - 1][col] != 0:
-            cell -= 1
-        if row + 1 < height and grid[row + 1][col] != 0:
-            cell -= 1
-        if col - 1 >= 0 and grid[row][col - 1] != 0:
-            cell -= 1
-        if col + 1 < width and grid[row][col + 1] != 0:
-            cell -= 1
-
-        return cell
-
-    return cell_perimeter
+ggrid = None
+gwidth, gheight = 0, 0
 
 
-def create_expand(grid, width, height):
-    """ Injects grid's dimensions """
-    def expand(row, col, calc_cell):
-        """ Expands the island searching for 1's """
-        if row < 0 or row >= height:
-            return 0
-        if col < 0 or col >= width:
-            return 0
+def cell_perimeter(row, col):
+    """ Calculates the perimeter of a cell """
+    cell = 4
+    if row - 1 >= 0 and ggrid[row - 1][col] != 0:
+        cell -= 1
+    if row + 1 < height and ggrid[row + 1][col] != 0:
+        cell -= 1
+    if col - 1 >= 0 and ggrid[row][col - 1] != 0:
+        cell -= 1
+    if col + 1 < width and ggrid[row][col + 1] != 0:
+        cell -= 1
 
-        perimeter = 0
+    return cell
 
-        cell = calc_cell(row, col)
-        perimeter += cell
 
-        # mark block cell as visited
-        grid[row][col] = -1
+def expand(row, col):
+    """ Expands the island searching for 1's """
+    if row < 0 or row >= height:
+        return 0
+    if col < 0 or col >= width:
+        return 0
 
-        if row - 1 >= 0 and grid[row - 1][col] == 1:
-            perimeter += expand(row - 1, col, calc_cell)
-        if row + 1 < height and grid[row + 1][col] == 1:
-            perimeter += expand(row + 1, col, calc_cell)
-        if col - 1 >= 0 and grid[row][col - 1] == 1:
-            perimeter += expand(row, col - 1, calc_cell)
-        if col + 1 < width and grid[row][col + 1] == 1:
-            perimeter += expand(row, col + 1, calc_cell)
+    perimeter = cell_perimeter(row, col)
+    # mark block cell as visited
+    ggrid[row][col] = -1
 
-        return perimeter
+    perimeter += expand(row - 1, col, calc_cell)
+    perimeter += expand(row + 1, col, calc_cell)
+    perimeter += expand(row, col - 1, calc_cell)
+    perimeter += expand(row, col + 1, calc_cell)
 
-    return expand
+    return perimeter
 
 
 def island_perimeter(grid):
@@ -59,17 +46,15 @@ def island_perimeter(grid):
     """
     # Grid is completely surrounded, there is one island or nothing
 
-    height = len(grid)
-    width = len(grid[0])
+    ggrid = grid
+    gheight = len(ggrid)
+    gwidth = len(ggrid[0])
 
     max_perimeter = 0
 
-    expand_island = create_expand(grid, width, height)
-    calc_cell = create_calc_cell(grid, width, height)
-
-    for row in range(height):
-        for col in range(width):
-            if grid[row][col] == 1:
+    for row in range(gheight):
+        for col in range(gwidth):
+            if ggrid[row][col] == 1:
                 return expand_island(row, col, calc_cell)
 
     return 0
